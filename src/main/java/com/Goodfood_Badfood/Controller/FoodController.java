a4d78368a3e13e50bb2c70e28f450c7d00e4b54a
504 <mz1693@naver.com>
2022-11-25 09:32:01 +0900

    Initial commit

---
+++ src/main/java/com/Goodfood_Badfood/Controller/FoodController.java
@@ -0,0 +1,42 @@
+package com.Goodfood_Badfood.Controller;
+
+import com.Goodfood_Badfood.service.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/foodinfo")
+public class FoodController {
+   // URL url = new URL("https://api.odcloud.kr/api/15039965/v1/uddi:7c77e65e-ef3f-4979-b69b-c79cfc15b45a?page=1&perPage=67&serviceKey=r8GL%2BVSaiHPTSkjqegmPHqGTnBPHbeGuSsWe4dc9ERWzLvxkjoCLUAzvnhYm6zssm5WuZXhl6%2FhQEdhiSbITJQ%3D%3D");
+
+    @Autowired
+    FoodService foodService;
+
+    @GetMapping("/badgood")
+    public Resource getbad(){
+        return new ClassPathResource("templates/Food.html");
+    }
+    @GetMapping("/kakaoMap")
+    public Resource getkakaoMap(){
+        return new ClassPathResource("templates/kakaoMap.html");
+    }
+
+    @GetMapping("/badfood")
+    public List badfood() throws IOException {
+        System.out.println("\n"+foodService.badfood());
+        return foodService.badfood();
+    }
+
+    @GetMapping("/goodfood")
+    public List goodfood() throws IOException {
+        System.out.println("\n"+foodService.goodfood());
+        return foodService.goodfood();
+    }
+}
---
+++ src/main/java/com/Goodfood_Badfood/service/FoodService.java
@@ -0,0 +1,62 @@
+package com.Goodfood_Badfood.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Service
+public class FoodService {
+    @Transactional
+    public List badfood() throws IOException {
+        List list = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
+
+        String urlStr = "https://api.odcloud.kr/api/15039965/v1/uddi:37c5ec4d-ce7a-47fc-8578-a4e9fdd7767c?page=1&perPage=521&serviceKey=r8GL%2BVSaiHPTSkjqegmPHqGTnBPHbeGuSsWe4dc9ERWzLvxkjoCLUAzvnhYm6zssm5WuZXhl6%2FhQEdhiSbITJQ%3D%3D";
+        URL url;
+        url = new URL(urlStr);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+
+        BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+        String rerutnLine;
+        while ((rerutnLine = br.readLine()) != null) {
+            result.append(rerutnLine + "\n\r");
+        }
+        urlConnection.disconnect();
+        list.add(result);
+        return list;
+    }
+
+
+    @Transactional
+    public List goodfood() throws IOException {
+        List list = new ArrayList<>();
+        StringBuilder result = new StringBuilder();
+
+        String urlStr = "https://api.odcloud.kr/api/3071314/v1/uddi:e4e7774d-0b16-4299-b830-dee5045df70f_201909291441?page=1&perPage=115&serviceKey=r8GL%2BVSaiHPTSkjqegmPHqGTnBPHbeGuSsWe4dc9ERWzLvxkjoCLUAzvnhYm6zssm5WuZXhl6%2FhQEdhiSbITJQ%3D%3D";
+        URL url;
+        url = new URL(urlStr);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+
+        BufferedReader br;
+        br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(), "UTF-8"));
+        String rerutnLine;
+        while ((rerutnLine = br.readLine()) != null) {
+            result.append(rerutnLine + "\n\r");
+        }
+        urlConnection.disconnect();
+        list.add(result);
+        return list;
+    }
+}
