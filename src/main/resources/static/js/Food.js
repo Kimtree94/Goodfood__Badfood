fb6f88c0270e48faf1894987e8eecc317828ae7a
최유정 <14dbwjddl@naver.com>
2022-12-01 09:11:08 +0900

    2022-11-30 모범음식점 마커 추가하려했음

--- src/main/resources/static/js/Food.js
+++ src/main/resources/static/js/Food.js
@@ -1,20 +1,24 @@
-let Lposition =[];
-let Rposition =[];
-
-
     /*************************************************카카오맵API******************************************************/
     var map = new kakao.maps.Map(document.getElementById('map'), { // 지도를 표시할 div
         center : new kakao.maps.LatLng(37.32186, 126.8308495), // 지도의 중심좌표
         level : 6 // 지도의 확대 레벨
     });
 
+
+
+var imageSrc ='/static/img/free-icon-heart-4551298.png', // 마커이미지의 주소입니다
+    imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+// 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
     // 마커 클러스터러를 생성합니다
     var clusterer = new kakao.maps.MarkerClusterer({
         map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
         averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-        minLevel: 10, // 클러스터 할 최소 지도 레벨
+        minLevel: 10, // 클러스터 할 최소 지도 레벨,
+        image: markerImage // 마커이미지 설정
     });
-
 
 badfood()
 /*위반 음식점 리스트*/
@@ -31,8 +35,6 @@ $.ajax({
         '<th>처분내용</th><th>처분일자</th><th>처분기간</th><th>관리기관</th><th>위도</th><th>경도</th><th>데이터기준일자</th>'+
         '</tr>'
         for(let i = 0 ; i<object.data.length; i++){
-             Lposition.push(object.data[i].위도)
-             Rposition.push(object.data[i].경도)
          html += '<tr>'+
          '<th>'+object.data[i].업종명+'</th><th>'+object.data[i].업소명+'</th><th>'+object.data[i].처분명+'</th><th>'+object.data[i].위반내용+'</th>'+
          '<th>'+object.data[i].처분내용+'</th><th>'+object.data[i].처분일자+'</th><th>'+object.data[i].처분기간+'</th><th>'+object.data[i].관리기관+'</th><th>'+object.data[i].위도+'</th><th>'+object.data[i].경도+'</th><th>'+object.data[i].데이터기준일자+'</th>'+
@@ -80,7 +82,8 @@ function getbadfood(){
             // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
             var markers = object.data.map( (e) => {
                  var marker =  new kakao.maps.Marker({
-                         position : new kakao.maps.LatLng(e.위도, e.경도)
+                         position : new kakao.maps.LatLng(e.위도, e.경도),
+                         markerImage
                     });
                    // 마커에 클릭 이벤트를 등록한다 (우클릭 : rightclick)
                     kakao.maps.event.addListener(marker, 'click', function() {
@@ -110,15 +113,12 @@ function getbadfood(){
 
                             info.innerHTML = fooghtml
                         }//모달내 내용 띄우기
-
                     });//카카오 이벤트 리스너
                   return marker;
              }  ) //markers end
-
             // 클러스터러에 마커들을 추가합니다
             clusterer.addMarkers(markers);
         });//
-
 }
 getgdfood()
 function getgdfood(){
@@ -170,11 +170,9 @@ function getgdfood(){
                     });//카카오 이벤트 리스너
                   return marker;
              }  ) //markers end
-
             // 클러스터러에 마커들을 추가합니다
             clusterer.addMarkers(markers);
         });//
-
 }
 
 /*==============================================================================================================*/
